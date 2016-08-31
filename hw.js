24ccc1652d93339c6668e96caf0a2db23f9e5951
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-08-31 12:31:21 -0400

    Expand solution to hit reach targets

--- hw.js
+++ hw.js
@@ -4,25 +4,40 @@
 
 var timerUI = {
   drawNumericDisplay: function(timerValue){
-    document.getElementById('numeric-display').textContent = timerValue;
+    var numericDisplay = document.getElementById('numeric-display');
+    numericDisplay.textContent = timerValue;
+    if (timerValue <= 10) {
+      numericDisplay.style.color = 'red';
+      var newSize = (1.5 * Math.pow(1.05, 11 - timerValue)).toFixed(2);
+      numericDisplay.style.fontSize = newSize + 'em';
+    }
   },
   drawProgressBars: function(timerValue){
-    var timeElapsed = 100 - timerValue;
-    document.getElementsByClassName('progress-bar')[0].style.width = timeElapsed + '%';
+    var progress = 100 - timerValue;
+    var bars = document.getElementsByClassName('progress-bar');
+    for (var i = 0; i < bars.length; i++) {
+      bars[i].style.width = progress + '%';
+    }
   },
   drawLitFuses: function(timerValue){
     var percentUnburnt = timerValue/100;
-    document.getElementsByClassName('unburnt')[0].style.width = percentUnburnt*98 + '%';
-    document.getElementsByClassName('burnt')[0].style.width = (1 - percentUnburnt)*98 + '%';
+    var numFuses = document.getElementsByClassName('fuse').length;
+    for (var i = 0; i < numFuses; i++){
+      document.getElementsByClassName('unburnt')[i].style.width = percentUnburnt*98 + '%';
+      document.getElementsByClassName('burnt')[i].style.width = (1 - percentUnburnt)*98 + '%';
+    }
   },
-  drawCrawlers: function(timerValue){
+  drawCrawlers: function(timerValue) {
     var timeElapsed = 100 - timerValue;
-    if (timerValue%2 === 0) {
-      document.getElementsByClassName('crawler')[0].style.marginTop = '0px';
+    var crawlers = document.getElementsByClassName('crawler');
+    for (var i = 0; i < crawlers.length; i++) {
+      if (timerValue%2 === 0) {
+        crawlers[i].style.marginTop = '0px';
+      }
+      else {
+        crawlers[i].style.marginTop = '10px';
+      }
+      crawlers[i].style.marginLeft = (timeElapsed*10) + 'px';
     }
-    else {
-      document.getElementsByClassName('crawler')[0].style.marginTop = '10px';
-    }
-    document.getElementsByClassName('crawler')[0].style.marginLeft = (timeElapsed*10) + 'px';
   }
 };
